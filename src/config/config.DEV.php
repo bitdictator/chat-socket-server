7e7fff5f79837f5174feb703f5229decc3be60c4
lextosounidis <lex.tosounidis@gmail.com>
2023-05-08 22:32:25 +0300

    Add letsencrypt cert and private key paths

--- src/config/config.DEV.php
+++ src/config/config.DEV.php
@@ -17,5 +17,8 @@ return [
     'CHAT_SOCKET_SERVER_CIPHER_KEY' => "sdkj478ksfdj83erhrui",
 
     // this is to be used with the OriginCheck which is not used
-    'ALLOWED_DOMAINS' => []
+    'ALLOWED_DOMAINS' => [],
+
+    'LOCAL_CERT_PATH' => '',    // Path to fullchain.pem
+    'LOCAL_PK_PATH' => ''       // Path to privkey.pem
 ];
