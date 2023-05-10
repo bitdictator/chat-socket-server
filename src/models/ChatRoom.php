65f48156f2800f6dccc631166a358a3db3a7b573
lextosounidis <lex.tosounidis@gmail.com>
2023-05-10 20:44:43 +0300

    Refactor ChatRoom class: replace SplObjectStorage with array, add type hints, and improve code readability

--- src/models/ChatRoom.php
+++ src/models/ChatRoom.php
@@ -7,12 +7,12 @@ use Core\Database;
 
 class ChatRoom
 {
-    private \SplObjectStorage $clients;
+    private array $clients;
 
     public function __construct(private int $chat_room_id, Client $first_client)
     {
-        $this->clients = new \SplObjectStorage;
-        $this->clients->attach($first_client);
+        $this->clients = [];
+        $this->clients[$first_client->getUserId()] = $first_client;
     }
 
     public function getId(): int
@@ -20,7 +20,14 @@ class ChatRoom
         return $this->chat_room_id;
     }
 
-    public function sendMessage(ConnectionInterface $from_conn, $text): bool
+    /**
+     * Sends a message to all clients in the chat room except the sender.
+     * 
+     * @param ConnectionInterface $from_conn The connection of the sender.
+     * @param string $text The message text.
+     * @return bool True on success, false on failure.
+     */
+    public function sendMessage(ConnectionInterface $from_conn, string $text): bool
     {
         $user_id = $this->getClientIdFromConnection($from_conn);
 
@@ -30,12 +37,10 @@ class ChatRoom
 
         $text = trim($text);
 
-        // send message with chat_id and user_id to bimboom database
         Database::insert('chat_line', ['chat_id' => $this->chat_room_id, 'sender_user_id' => $user_id, 'message' => $text]);
 
-        // send message to all clients connected to this chat room
-        foreach ($this->clients as $client) {
-            if ($from_conn !== $client->getConnection()) {   // send to others, not self
+        foreach ($this->clients as $client_id => $client) {
+            if ($client->getConnection() !== $from_conn) {
                 $client->sendMessage(json_encode([
                     'text' => htmlspecialchars($text),
                     'sent_at' => date('Y-m-d H:i:s')
@@ -48,15 +53,15 @@ class ChatRoom
 
     public function addClient(Client $client_to_add): bool
     {
-        $this->clients->attach($client_to_add);
+        $this->clients[$client_to_add->getUserId()] = $client_to_add;
         return true;
     }
 
     public function getClientIdFromConnection(ConnectionInterface $conn): int|false
     {
-        foreach ($this->clients as $client) {
+        foreach ($this->clients as $client_id => $client) {
             if ($conn === $client->getConnection()) {
-                return $client->getUserId();
+                return $client_id;
             }
         }
 
@@ -65,55 +70,39 @@ class ChatRoom
 
     public function getClientIDs(): array
     {
-
-        $client_ids = [];
-
-        $count = 0;
-        foreach ($this->clients as $client) {
-            $client_ids[$count] = $client->getUserId();
-            $count++;
-        }
-
-        return $client_ids;
+        return array_keys($this->clients);
     }
 
     public function hasClient(Client $client): bool
     {
-        return $this->clients->contains($client);
+        return isset($this->clients[$client->getUserId()]);
     }
 
     public function hasClientWithConnection(ConnectionInterface $conn): bool
     {
-        foreach ($this->clients as $client) {
-            if ($client->getConnection() === $conn) {   // find if this room has the client with the given conneciton
-                return true;
-            }
-        }
-
-        return false;
+        return $this->getClientIdFromConnection($conn) !== false;
     }
 
     public function removeClient(Client $client): bool
     {
-        $this->clients->detach($client);
+        unset($this->clients[$client->getUserId()]);
         return true;
     }
 
     public function removeClientWithConnection(ConnectionInterface $conn): bool
     {
-        // find and delete the client with given connection
-        foreach ($this->clients as $client) {
-            if ($client->getConnection() === $conn) {
-                $this->clients->detach($client);
-                return true;
-            }
+        $user_id = $this->getClientIdFromConnection($conn);
+
+        if ($user_id !== false) {
+            unset($this->clients[$user_id]);
+            return true;
         }
 
-        return true;
+        return false;
     }
 
     public function getClientsCount(): int
     {
-        return $this->clients->count();
+        return count($this->clients);
     }
 }
